bf360bd784a4001bec80e8d001158ce98a53bdb7
ShavkatMoskvin <Shavkatmoskvin@gmail.com>
2021-10-08 11:20:10 +0300

    исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -6,35 +6,18 @@ const nameText = document.querySelector(".profile__subtitle");
 const popupField = document.querySelector(".popup__input_name");
 const popupFieldText = document.querySelector(".popup__input_text");
 const formButton = document.querySelector(".popup__button");
-const elementLike = document.querySelector(".element__like");
 
-//popupFieldText = nameText;
-//;
-
-//document.getElementByIdelementLike.style.backgroundImage = 'url(../../../images/Union.svg)';
-elementLike.addEventListener("click", function () {
-  elementLike.style.backgroundImage = "url(../../../images/Union.svg)";
-  event.preventDefault()
-});
 function closePopup() {
   popup.classList.remove("popup__isOpen");
 }
 function openPopup() {
   popupField.value = Name.textContent;
-  popupFieldText.value = nameText.textContent
-  popup.classList.add("popup__isOpen")
-  ;
+  popupFieldText.value = nameText.textContent;
+  popup.classList.add("popup__isOpen");
 }
 
 editButton.addEventListener("click", openPopup);
 popupClose.addEventListener("click", closePopup);
-
-//function submitForm(event) {
-
-//nameText.textContent = formButton.nodeValue;
-//event.preventDefault();
-//closePopup()
-//}
 
 formButton.addEventListener("click", function (event) {
   Name.textContent = popupField.value;
